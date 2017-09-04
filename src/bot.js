0121ea4a7000bec8d5ce0f8e92cb39fd844311ef
Joca <jpereyraleon@gmail.com>
2017-09-04 15:35:53 -0500

    flow complete

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,9 @@
 'use strict';
 const BootBot = require('bootbot');
+const moment = require('moment');
 const face = require('./services/face');
 require('dotenv').config()
+moment.locale('es');
 
 const bot = new BootBot({
   accessToken: process.env.PAGE_ACCESS_TOKEN,
@@ -21,47 +23,84 @@ bot.start(process.env.PORT || 3000);
 
 const askForPhoto = (convo) => {
   convo.ask('Envíame una foto tuya o de la persona que realizará la denuncia', (payload, convo) => {
-    if(payload.message.attachments) {
-      face.detectFace(payload.message);
+    if (payload.message.attachments) {
+      face.detectFace(payload.message).then((response) => {
+        if (response.data != []) {
+          face.identifyPerson(response.data[0].faceId).then((response) => {
+            if (response.data[0].candidates != []) {
+              face.getPersonInformation(response.data[0].candidates[0].personId).then((response) => {
+                convo.say(`Hola, ${response.data.name}.`).then(() => {
+                  convo.set('person', response.data);
+                  askForLocation(convo);
+                });
+              });
+            }
+          });
+        } else {
+          convo.say('No has enviado una foto con un rostro reconocible.').then(() => {
+            askForPhoto(convo);
+          });
+        }
+      });
     } else {
       convo.say('No has enviado una foto.').then(() => {
         askForPhoto(convo);
-      })
+      });
     }
   });
 }
 
-// function receivedMessage(event) {
-//   var senderId = event.sender.id;
-//   axios.defaults.headers.post['Ocp-Apim-Subscription-Key'] = process.env.AZURE_KEY;
-//   axios.defaults.headers.get['Ocp-Apim-Subscription-Key'] = process.env.AZURE_KEY;
+const askForLocation = (convo) => {
+  convo.ask({
+    text: 'Ahora envíanos la ubicación del hecho sucedido',
+    quickReplies: [{
+        'content_type': 'location'
+      }]
+  }, (payload, convo) => {
+      convo.set('location', payload.message.attachments[0].payload.coordinates);
+      askForDate(convo);
+  });
+}
 
-//   if(event.message.attachments) {
-//     axios.post('https://eastus2.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false', {
-//       'url': event.message.attachments[0].payload.url
-//     }).then(function(response) {
-//       axios.post('https://eastus2.api.cognitive.microsoft.com/face/v1.0/identify', {
-//         'personGroupId': 'person-data',
-//         'faceIds': [response.data[0].faceId],
-//         'maxNumOfCandidatesReturned': 1,
-//         'confidenceThreshold': 0.5
-//       }).then(function(response) {
-//         if(response.data[0].candidates.length > 0) {
-//           axios.get('https://eastus2.api.cognitive.microsoft.com/face/v1.0/persongroups/person-data/persons/'+response.data[0].candidates[0].personId)
-//                .then(function(response) {
-//                 callSendAPI(senderId, `Hola ${response.data.name}`);
-//                }).catch(function (error) {
-//                  console.log(error);
-//                });
-//         } else {
-//           callSendAPI(senderId, 'No se encontró un registro con su foto.');
-//         }
-//       }).catch(function (error) {
-//         console.log(error);
-//       });
-//     }).catch(function (error) {
-//       console.log(error);
-//     });
-//   }
+const askForDate = (convo) => {
+  convo.ask(`Envíanos una fecha co formato día/mes/año. Por ejemplo el día de hoy es ${moment().format('DD/MM/YYYY')}`, (payload, convo) => {
+      let date = payload.message.text;
+      date = moment(date, 'dd/mm/yyyy');
+      if (moment(date, 'dd/mm/yyyy').isValid()) {
+        convo.say(`La fecha escogida es ${date.format('LL')}`).then(() => {
+          convo.set('date', date.format('X'));
+          askForAditionalInformation(convo);
+        });
+      } else {
+        convo.say(`Hubo un error con la fecha ingresada.`).then(() => {
+          askForDate(convo);
+        });
+      }
+  });
+}
 
-// }
+const askForAditionalInformation = (convo) => {
+  convo.ask(`Deseas agregar información adicional?`, (payload, convo) => {
+    let coordinates = convo.get('location');
+    convo.set('aditionalInformation', payload.message.text);
+    sendSummary(convo);
+  });
+}
+
+const sendSummary = (convo) => {
+  let date = moment.unix(convo.get('date'));
+  let coordinates = convo.get('location');
+
+  convo.say(`La información que se envío es la siguiente:
+  - Denunciante: ${convo.get('person').name}
+  - Fecha: ${date.format('LL')}
+  - Información adicional: ${convo.get('aditionalInformation')}
+  - Ubicación: `).then(() => {
+    convo.say({
+      attachment: 'image',
+      url: `https:\/\/maps.googleapis.com\/maps\/api\/staticmap?size=764x400&center=${coordinates.lat},${coordinates.long}&zoom=18&markers=${coordinates.lat},${coordinates.long}`,
+    });
+
+    convo.end();
+  });
+}
--- src/services/face.js
+++ src/services/face.js
@@ -1,12 +1,25 @@
 const axios = require('axios');
 require('dotenv').config()
-const apiBase = 'https://eastus2.api.cognitive.microsoft.com/face/v1.0/';
+const apiBase = 'https://eastus2.api.cognitive.microsoft.com/face/v1.0';
 axios.defaults.headers.post['Ocp-Apim-Subscription-Key'] = process.env.AZURE_KEY;
 axios.defaults.headers.get['Ocp-Apim-Subscription-Key'] = process.env.AZURE_KEY;
 
 exports.detectFace = (message) => {
-    return axios.post(apiBase + 'detect?returnFaceId=true&returnFaceLandmarks=false', {
+    return axios.post(apiBase + '/detect?returnFaceId=true&returnFaceLandmarks=false', {
       'url': message.attachments[0].payload.url
     });
+}
+
+exports.identifyPerson = (faceId) => {
+    return axios.post(apiBase + '/identify', {
+        'personGroupId': 'person-data',
+        'faceIds': [faceId],
+        'maxNumOfCandidatesReturned': 1,
+        'confidenceThreshold': 0.5
+    });
+}
+
+exports.getPersonInformation = (personId) => {
+    return  axios.get(`${apiBase}/persongroups/person-data/persons/${personId}`);
 }
 
