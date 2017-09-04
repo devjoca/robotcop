a1d68aac0f9034fe1530e3cf16da39b9f1a7c23e
Joca <jpereyraleon@gmail.com>
2017-09-04 02:03:25 -0500

    add hi

--- src/bot.js
+++ src/bot.js
@@ -13,6 +13,14 @@ bot.on('message', (payload, chat) => {
   chat.say(`Echo: ${text}`);
 });
 
+bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+  // Send a text message followed by another text message that contains a typing indicator
+  chat.say('Hello, human friend!').then(() => {
+    chat.say('How are you today?', { typing: true });
+  });
+});
+
+
 bot.start();
 
 // function receivedMessage(event) {
