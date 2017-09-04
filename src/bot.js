b6c0fa88ec360f303d5093322105be1616ab1719
Joca <jpereyraleon@gmail.com>
2017-09-04 02:25:38 -0500

    add examples

--- src/bot.js
+++ src/bot.js
@@ -20,6 +20,33 @@ bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
   });
 });
 
+bot.hear(['food', 'hungry'], (payload, chat) => {
+  // Send a text message with quick replies
+  chat.say({
+    text: 'What do you want to eat today?',
+    quickReplies: ['Mexican', 'Italian', 'American', 'Argentine']
+  });
+});
+
+bot.hear(['help'], (payload, chat) => {
+  // Send a text message with buttons
+  chat.say({
+    text: 'What do you need help with?',
+    buttons: [
+      { type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
+      { type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
+      { type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
+    ]
+  });
+});
+
+bot.hear('image', (payload, chat) => {
+  // Send an attachment
+  chat.say({
+    attachment: 'image',
+    url: 'http://example.com/image.png'
+  });
+});
 
 bot.start(process.env.PORT || 3000);
 
